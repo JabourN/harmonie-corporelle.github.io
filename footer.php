053f1fe45806fd118e9001b575f43b50edcc3908
58381 <najoua.jabour@isfsc.be>
2023-11-15 18:42:45 +0100

    Footer + Header + Nav

---
+++ footer.php
@@ -0,0 +1,23 @@
+<!-- footer -->
+<div class="container overflow-hidden text-center">
+  <div class="row gy-5">
+    <div class="col-6">
+      <div class="p-3"><?php echo 'NEWSLETTER'; ?></div>
+    </div>
+    <div class="col-6">
+      <div class="p-3"><?php echo 'POSTULER'; ?></div>
+    </div>
+    <div class="col-6">
+      <div class="p-3"><?php echo 'COMMENTAIRE'; ?></div>
+    </div>
+    <div class="col-6">
+      <div class="p-3"><?php echo 'CONTACT'; ?></div>
+    </div>
+    <div class="col-6">
+      <div class="p-3"><?php echo 'NUMERO'; ?></div>
+    </div>
+    <div class="col-6">
+      <div class="p-3"><?php echo 'MAIL'; ?></div>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
+<!-- Accueil -->
 <?php get_header(); ?>
-<?php
-    $cheminImage = 'assets/images/ICONE_INSTAGRAM.png';
-    $cheminImage = 'assets/images/ICONE_PINTEREST.png';
-    $cheminImage = 'assets/images/ICONE_PODCAST.png';
-?>
+<?php include('nav.php'); ?>
+<?php get_footer(); ?>
