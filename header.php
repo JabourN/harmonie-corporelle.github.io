8541841f9af2a6edef6b23c8dc21297c52356006
58381 <najoua.jabour@isfsc.be>
2023-11-21 19:26:04 +0100

    Correction Navbar + index + header

--- header.php
+++ header.php
@@ -12,8 +12,10 @@
   <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> >
 <header>
+
+<!-- Les icônes -->
 <a class="icon-link" href="https://www.instagram.com/harmonie.corporellee/">
 <img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/ICONE_INSTAGRAM.png ">
 </a>
@@ -23,8 +25,13 @@
 <a class="icon-link" href="#">
 <img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/ICONE_PODCAST.png ">
 </a>
-</header>
 
+<!-- Le logo -->
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/LOGO.png" 
+class="rounded mx-auto d-block" alt="LOGO">
+
+</header>
+<!-- Script de bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
-</html>
--- index.php
+++ index.php
@@ -1,9 +1,43 @@
 <!-- Accueil -->
 <?php get_header(); ?> 
 
-<?php include('nav.php'); ?>
-
 <!-- Contenu principal -->
+<body>
 
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ACCUEIL</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">CONSEIL</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">IMC</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">FORUM</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            En savoir plus
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Ressources</a></li>
+            <li><a class="dropdown-item" href="#">Instagram</a></li>
+            <li><a class="dropdown-item" href="#">Experts</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
+<!-- MAIN -->
+
+</body>
 <?php get_footer() ?>
