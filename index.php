4063cdb8db717e95562472f645a80512cc1b53fa
58381 <najoua.jabour@isfsc.be>
2023-12-11 15:32:09 +0100

    modification images caroussel

--- index.php
+++ index.php
@@ -51,16 +51,16 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
     <a class="icon-link" href="https://getbootstrap.com/docs/5.3/components/carousel/#how-it-works">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Conseil.png" class="d-block w-100" alt="Conseil">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/sondage.png" class="d-block w-100" alt="Conseil">
     </a>
     </div>
     <div class="carousel-item">
     <a class="icon-link" href="https://getbootstrap.com/docs/5.3/components/carousel/#how-it-works">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/imc.png" class="d-block w-100" alt="IMC">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/IMC.png" class="d-block w-100" alt="IMC">
     </a>    </div>
     <div class="carousel-item">
     <a class="icon-link" href="https://getbootstrap.com/docs/5.3/components/carousel/#how-it-works">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Forum.png" class="d-block w-100" alt="Forum">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/forum.png" class="d-block w-100" alt="Forum">
     </a>    </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -107,36 +107,61 @@
 </div>
 </div>
 <!-- Partie 3 : Audio & book !!!! RAJOUTER DES COLONNES PARCE QUE C'EST LAID !!!-->
-<div class="card mb-3" style="max-width: 540px;">
+<div class="container card mb-3" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-4">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/casque.png" style="width: 270px; class="d-block w-100" alt="casque">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/casque.png"  class=" img-fluid" alt="podcast">
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text text-center">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text text-center"></p>
+        <h5 class="card-title">PODCAST</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>
 </div>
 
-<div class="card mb-3" style="max-width: 540px;">
+<div class="container card mb-3" style="max-width: 540px;">
   <div class="row g-0">
-    <div class="col-md-4">    <h5 class="card-title">Card title</h5>
+    <div class="col-md-4">    
+      <h5 class="card-title text-center">LIVRES</h5>
         <p class="card-text text-center">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text text-center"></p>
+        <p class="card-text "></p>
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/livre.png" style="width:500px;" class="d-block w-100" alt="livre">
-
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/livre.png" class="d-block w-100" alt="livre">
+        <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>
 </div>
 
+<!-- Partie 4 : Commentaire -->
+<div class="container">
+  <div id="carouselExampleRide" class="carousel slide mx-auto" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="container carousel-item active">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/commentaire1.png" class="d-block w-100 img-fluid" alt="commentaire" style="max-width: 700px; height: auto;">
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/commentaire2.png" class="d-block w-100 img-fluid" alt="commentaire" style="max-width: 700px; height: auto;">
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/commentaire3.png" class="d-block w-100 img-fluid" alt="commentaire" style="max-width: 700px; height: auto;">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
 
 </main>
 </body>
