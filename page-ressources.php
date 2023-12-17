641c24c6a744a4d5e9312be66cfa560997b07c62
Salsabil27 <Aberkan.s@hotmail.com>
2023-12-17 17:45:30 +0100

    page ressources

--- page-ressources.php
+++ page-ressources.php
@@ -1,3 +1,5 @@
+<?php get_header(); ?>
+
 <?php
 // Déterminer la date à afficher en fonction du paramètre 'date' dans l'URL
 if (isset($_GET['date'])) {
@@ -21,17 +23,45 @@ $args = array(
 $articles = new WP_Query($args);
 ?>
 
-<div id="content">
-    <?php while ($articles->have_posts()) : $articles->the_post(); ?>
-        <!-- Afficher le contenu de l'article ici -->
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-    <?php endwhile; ?>
+<div id="navigation" class="text-center">
+    <a href="?date=<?php echo date('Ymd', strtotime('-1 day', strtotime($date_actuelle))); ?>"><</a>
+    <span><?php echo date('d/m/Y', strtotime($date_actuelle)); ?></span>
+    <a href="?date=<?php echo date('Ymd', strtotime('+1 day', strtotime($date_actuelle))); ?>">></a>
 </div>
 
-<div id="navigation">
-    <a href="?date=<?php echo date('Ymd', strtotime('-1 day', strtotime($date_actuelle))); ?>"><</a>
-    <a href="?date=<?php echo date('Ymd', strtotime('+1 day', strtotime($date_actuelle))); ?>">></a>
+<div id="content" class="text-center"> 
+    <?php while ($articles->have_posts()) : $articles->the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+
+        <?php
+        // Récupérer le type de l'article (podcast, livre, video)
+        $type_article = get_post_meta(get_the_ID(), 'type_article', true);
+
+        // Appliquer des styles différents en fonction du type d'article
+        switch ($type_article) {
+            case 'podcast':
+                // Ajouter une classe pour les articles de type podcast
+                echo '<div class="article-content podcast-content">';
+                break;
+            case 'livre':
+                // Ajouter une classe pour les articles de type livre
+                echo '<div class="article-content livre-content">';
+                break;
+            case 'video':
+                // Ajouter une classe pour les articles de type vidéo
+                echo '<div class="article-content video-content">';
+                break;
+            default:
+                // Ajouter une classe par défaut pour les autres types d'articles
+                echo '<div class="article-content default-content">';
+                break;
+        }
+        ?>
+
+        <?php the_content(); ?>
+
+        </div>
+    <?php endwhile; ?>
 </div>
 
 <?php get_footer(); ?>
